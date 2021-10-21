cf682e69dc2d3c7334350e7e11d05a94f259f40c
D.Boy.Blade <ykw0104@163.com>
2021-10-21 16:06:43 +0800

    first commit

--- README.md
+++ README.md
@@ -17,3 +17,9 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 网页云音乐API
+
+#### api地址: https://neteasecloudmusicapi.vercel.app/
+
+#### github地址: https://github.com/Binaryify/NeteaseCloudMusicApi
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="d1"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+#d1 {
+  width: 3.75rem;
+  height: 3.75rem;
+  background-color: red;
+}
+</style>
